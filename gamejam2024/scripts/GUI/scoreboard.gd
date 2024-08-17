4a3565593e1e25262596f1bcff4c6cc08553e943
Kevin Lam <kplam624@gmail.com>
2024-08-17 13:52:30 -0400

    Added the scoreboard and some scoring logic.

--- gamejam2024/scripts/GUI/scoreboard.gd
+++ gamejam2024/scripts/GUI/scoreboard.gd
@@ -1,16 +1,13 @@
 extends Node
 
-var score = 0
-
-func _on_fish_eaten():
-	score += 1
-	
+var fish_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Label.text = "Score: " + str(fish_score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	fish_score = System.score
+	$Label.text = "Score: " + str(fish_score)
+	
--- gamejam2024/scripts/system/system.gd
+++ gamejam2024/scripts/system/system.gd
@@ -5,3 +5,6 @@ enum PLAYER_STATES {
 	CHARGE,
 	DASH
 }
+
+# To keep track of the player's score over time.
+var score = 0
