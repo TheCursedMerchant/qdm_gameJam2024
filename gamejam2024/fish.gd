889a165894c42de6da0db406925544242c73e3b3
Kevin Lam <kplam624@gmail.com>
2024-08-16 21:37:39 -0400

    Added size modifiers to the fish enemies.

--- gamejam2024/fish.gd
+++ gamejam2024/fish.gd
@@ -1,13 +1,19 @@
 extends "res://scripts/food/food.gd"
 
 @export_category("movement")
-@export var speed : float = 20.0
+@export var speed : float = 2.0
 @export var direction := Vector2.RIGHT
 @export var size_scale := Vector2(1, 1)
 
+func random_size_scale():
+	var size_scale := randf_range(0,2)
+	return Vector2(size_scale, size_scale)
+	
 func _ready() -> void:
-	sprite.scale = size_scale
-	collisionShape.scale = size_scale
-
+	#sprite.scale = size_scale
+	#collisionShape.scale = size_scale
+	sprite.scale = random_size_scale()
+	collisionShape.scale = random_size_scale()
+	
 func _physics_process(delta: float) -> void:
 	global_position += direction * speed
