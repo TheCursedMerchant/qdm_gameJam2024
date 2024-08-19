fc653461bfe55561dc24e0124b67ea2d4cd1350c
Kevin Lam <kplam624@gmail.com>
2024-08-19 00:51:54 -0400

    Event listeners and extending to the food class.

--- gamejam2024/scripts/npc/hunter_fish.gd
+++ gamejam2024/scripts/npc/hunter_fish.gd
@@ -1,8 +1,9 @@
 class_name Hunter
-extends CharacterBody2D
+extends "res://scripts/food/food.gd"
 
 @export var direction := Vector2.RIGHT
 @export var baseSpeed = 300
+@export var shrinkValue = 0.05
 
 @onready var huntingTimer:= $HuntTimer
 
@@ -12,17 +13,28 @@ var player: CharacterBody2D
 
 func _ready() -> void:
 	huntingTimer.start()
+	huntingTimer.connect('timeout',_on_hunt_timer_timeout)
+	connect("body_entered",_on_body_entered_hunter)
+	player = System.player_body
 	
 func _process(delta: float) -> void:
-	fish_movement()
+	fish_movement(delta)
 
-func fish_movement():
+func fish_movement(delta):
 	if isHunting:
-		player = System.player_body
-		velocity = position.direction_to(player.position) * baseSpeed
+		global_position += position.direction_to(player.position) * baseSpeed * delta
 	else:
-		velocity = direction * baseSpeed
-	move_and_slide()
+		global_position += direction * baseSpeed * delta
 	
 func _on_hunt_timer_timeout() -> void:
 	isHunting = true
+	
+func _on_body_entered_hunter(body: Node2D):
+	if body.get_groups().has("Player"):
+		var player : Player = body
+		if (!player.isRecovery):
+			#player.grow(-shrinkValue)
+			#player.take_damage()
+			isHunting = false
+			huntingTimer.start()
+	
